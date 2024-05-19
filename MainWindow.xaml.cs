4b2e2245fa3b8e29735dc51904bf1b9f925a3da4
siiyyko <oleksii.blyzniuk.pz.2022@lpnu.ua>
2024-05-19 18:45:29 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,321 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace lab_05
+{
+    public partial class MainWindow : Window
+    {
+        private double gridSize = 30;
+        private double speed;
+        private int fps;
+
+        private TriangleDrawer? triangle;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DealWithGrid();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            DealWithGrid();
+        }
+
+        #region BasicGridSettings
+        private void DealWithGrid()
+        {
+            DrawAxes();
+            TransormCoordinateSystem();
+        }
+
+        private void DrawAxes()
+        {
+            axisCanvas.Children.Clear();
+
+            double width = axisCanvas.ActualWidth;
+            double height = axisCanvas.ActualHeight;
+
+            Line xAxis = new();
+            Line yAxis = new();
+
+
+            xAxis.Stroke = Brushes.Black;
+            yAxis.Stroke = Brushes.Black;
+
+            xAxis.StrokeThickness = 2;
+            yAxis.StrokeThickness = 2;
+
+            xAxis.X1 = 0;
+            xAxis.Y1 = height / 2;
+            xAxis.X2 = width;
+            xAxis.Y2 = height / 2;
+
+            yAxis.X1 = width / 2;
+            yAxis.Y1 = 0;
+            yAxis.X2 = width / 2;
+            yAxis.Y2 = height;
+
+            axisCanvas.Children.Add(xAxis);
+            axisCanvas.Children.Add(yAxis);
+
+            GenerateAxisNumbers();
+
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            double width = axisCanvas.ActualWidth;
+            double height = axisCanvas.ActualHeight;
+
+            for (double i = -height / 2; i <= 0; i += gridSize)
+            {
+                Line line = new Line()
+                {
+                    Opacity = 0.3,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.5,
+                    X1 = 0,
+                    X2 = width,
+                    Y1 = i + height,
+                    Y2 = i + height
+                };
+
+                axisCanvas.Children.Add(line);
+            }
+
+            for (double i = height / 2; i >= 0; i -= gridSize)
+            {
+                Line line = new Line()
+                {
+                    Opacity = 0.3,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.5,
+                    X1 = 0,
+                    X2 = width,
+                    Y1 = i,
+                    Y2 = i
+                };
+
+                axisCanvas.Children.Add(line);
+            }
+
+            for (double i = -width / 2; i <= 0; i += gridSize)
+            {
+                Line line = new Line()
+                {
+                    Opacity = 0.3,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.5,
+                    X1 = i + width,
+                    X2 = i + width,
+                    Y1 = 0,
+                    Y2 = height
+                };
+
+                axisCanvas.Children.Add(line);
+            }
+
+            for (double i = width / 2; i >= 0; i -= gridSize)
+            {
+                Line line = new Line()
+                {
+                    Opacity = 0.3,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.5,
+                    X1 = i,
+                    X2 = i,
+                    Y1 = 0,
+                    Y2 = height
+                };
+
+                axisCanvas.Children.Add(line);
+            }
+        }
+
+        private void GenerateAxisNumbers()
+        {
+            double width = axisCanvas.ActualWidth;
+            double height = axisCanvas.ActualHeight;
+
+            double quantOfX = width / gridSize;
+            double quantOfY = height / gridSize;
+
+            for (int i = (int)(-(quantOfX / 2)); i <= (quantOfX / 2); ++i)
+            {
+                TextBlock xText = new TextBlock()
+                {
+                    Text = i.ToString(),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Margin = new Thickness((i + (quantOfX / 2)) * gridSize, height / 2, 0, 0),
+                    FontSize = gridSize / 2.5
+                };
+                axisCanvas.Children.Add(xText);
+            }
+
+            for (int i = (int)((quantOfY / 2)); i >= (-quantOfY / 2); --i)
+            {
+                TextBlock yText = new TextBlock()
+                {
+                    Text = i.ToString(),
+                    HorizontalAlignment = HorizontalAlignment.Right,
+                    VerticalAlignment = VerticalAlignment.Top,
+                    Margin = new Thickness(width / 2, (-i + (quantOfY / 2)) * gridSize, 0, 0),
+                    FontSize = gridSize / 2.5
+                };
+                axisCanvas.Children.Add(yText);
+            }
+        }
+
+        private void TransormCoordinateSystem()
+        {
+            double centerX = axisCanvas.ActualWidth / 2;
+            double centerY = axisCanvas.ActualHeight / 2;
+
+            TranslateTransform transform = new TranslateTransform(centerX, centerY);
+            mainCanvas.RenderTransform = transform;
+        }
+        #endregion
+
+        private void GridCell_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            gridSize = e.NewValue;
+            DealWithGrid();
+            triangle?.HandleGridSizeChange(e.OldValue, e.NewValue);
+
+        }
+
+        private void MotionSpeed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            speed = e.NewValue;
+        }
+
+        private async void StartMotion_Click(object sender, RoutedEventArgs e)
+        {
+            if(!double.TryParse(StepsTextBox.Text, out double steps) || !double.TryParse(ScaleTextBox.Text, out double scale)){
+                MessageBox.Show("Please, check typed parameters!");
+                return;
+            }
+            if(triangle is null)
+            {
+                return;
+            }
+
+            //1 - speed, 2 - cells to move, 3 - scaleFactor, 4 - fps
+            await triangle.StartAnimation(1000/speed, steps*gridSize, scale, (1000/fps));
+        }
+
+        private void StopMotion_Click(object sender, RoutedEventArgs e)
+        {
+            if (triangle is null)
+            {
+                return;
+            }
+            triangle.StopAnimation();
+        }
+
+        private void ResetMotion_Click(object sender, RoutedEventArgs e)
+        {
+            if (triangle is null)
+            {
+                return;
+            }
+            triangle.ResetAnimation();
+        }
+
+        private void SaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            double maxWidth = Math.Max(mainCanvas.ActualWidth, axisCanvas.ActualWidth);
+            double maxHeight = Math.Max(mainCanvas.ActualHeight, axisCanvas.ActualHeight);
+
+            // Create a RenderTargetBitmap to capture the combined content
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(
+                (int)maxWidth, (int)maxHeight, 96, 96, PixelFormats.Pbgra32);
+
+            // Render both canvases onto the bitmap (order doesn't matter here)
+            renderTargetBitmap.Render(axisCanvas);
+            renderTargetBitmap.Render(mainCanvas);
+            string fileName = "canvas_image.png"; // Replace with desired filename and extension (png, jpg, etc.)
+            using (FileStream outputStream = File.Create(fileName))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder(); // Choose encoder based on desired format
+                encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+                encoder.Save(outputStream);
+            }
+
+        }
+
+        private void SaveMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            if (triangle is null)
+            {
+                return;
+            }
+            triangle.SendResultTransformMatrixToFile();
+        }
+
+        private void DrawTriangle_Click(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Children.Clear();
+
+            string[] vertexes =
+            {
+                Vertex1_TextBox.Text,
+                Vertex2_TextBox.Text,
+                Vertex3_TextBox.Text,
+            };
+
+            List<Point> points = [];
+            foreach(var vertex in vertexes)
+            {
+                string[] aloneCoords = vertex.Split(' ');
+                List<double> coords = new();
+                foreach(var coord in aloneCoords)
+                {
+                    if(!double.TryParse(coord, out var c))
+                    {
+                        MessageBox.Show("Please, check typed coordinates!");
+                        return;
+                    }
+                    coords.Add(c);
+                    
+                }
+                if (coords.Count != 2) return;
+
+                points.Add(new Point(coords[0] * gridSize, coords[1] * (-gridSize)));
+            }
+
+            triangle = new(points, speed, mainCanvas);
+
+            triangle.Draw();
+        }
+
+        public static Window GetMainWindowController()
+        {
+            var mn = Application.Current.MainWindow as MainWindow ?? throw new Exception("Can't get MainWindow");
+            return mn;
+        }
+
+        private void FPS_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            fps = (int)e.NewValue;
+        }
+    }
+}
---
+++ TriangleDrawer.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Timers;
+using System.Windows.Threading;
+using System.IO;
+
+namespace lab_05
+{
+    internal class TriangleDrawer
+    {
+        private List<Point> points;
+        private Polygon? triangle;
+        public double Speed { get; set; }
+        private Canvas canvas;
+
+        private bool isStopped = false;
+        public int LastStep { get; private set; }
+        private List<Point> lastPoints;
+
+        string resultTransformMatrix;
+
+        private double stepsAlongLine;
+        private double scaleFactor;
+
+        public TriangleDrawer(List<Point> points, double speed, Canvas canvas)
+        {
+            this.points = points;
+            Speed = speed;
+            this.canvas = canvas;
+        }
+
+        public void HandleGridSizeChange(double prevVal, double newVal)
+        {
+            for(int i = 0; i < points.Count; i++)
+            {
+                Point p = new()
+                {
+                    X = points[i].X * (newVal / prevVal),
+                    Y = points[i].Y * (newVal / prevVal)
+                };
+                points[i] = p;
+            }
+
+            canvas.Children.Clear();
+            Draw();
+        }
+
+        public void Draw()
+        {
+            triangle = new()
+            {
+                Points = new PointCollection(points),
+                Fill = Brushes.Blue,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1,
+            };
+
+            canvas.Children.Clear();
+            canvas.Children.Add(triangle);
+        }
+
+
+        public async Task StartAnimation(double steps, double units, double scale, int fps)
+        {
+            var mw = MainWindow.GetMainWindowController();
+            List<Point> newPoints = points.Select(p => p).ToList();
+            lastPoints = newPoints.Select(p => p).ToList();
+            GenerateResultTransformMatrix(units, scale);
+            for (int k = LastStep; k < steps; ++k)
+            {
+                isStopped = false;
+                for (int i = 0; i < newPoints.Count; i++)
+                {
+                    newPoints[i] = Translate(newPoints[i], units / steps, -units / steps);
+                    newPoints[i] = Scale(newPoints[i], Math.Pow(scale, 1 / steps), Math.Pow(scale, 1 / steps));
+                }
+                points = newPoints;
+                mw.Dispatcher.Invoke(() =>
+                {
+                    Draw();
+
+                });
+                await Task.Delay(fps);
+
+                if (isStopped)
+                {
+                    LastStep = k;
+                    return;
+                }
+            }
+            
+        }
+
+        public void StopAnimation()
+        {
+            isStopped = true;
+        }
+
+        public void ResetAnimation()
+        {
+            points = lastPoints;
+            Draw();
+        }
+
+        private static double[,] MatrixMultiply(double[,] matrix, double[,] col)
+        {
+            if (matrix.GetLength(1) != col.GetLength(0))
+            {
+                throw new ArgumentException("Incompatible matrix dimensions for multiplication.");
+            }
+
+            int rowsA = matrix.GetLength(0);
+            int colsB = col.GetLength(1);
+            double[,] result = new double[rowsA, colsB];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < matrix.GetLength(1); k++)
+                    {
+                        result[i, j] += matrix[i, k] * col[k, j];
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private Point Translate(Point p, double dx, double dy)
+        {
+            double[,] translateMatrix =
+            {
+                {1, 0, dx },
+                {0, 1, dy },
+                {0, 0, 1 }
+            };
+
+            double[,] pointRaw =
+            {
+                { p.X },
+                { p.Y },
+                { 1 }
+            };
+
+            double[,] pointTransformed = MatrixMultiply(translateMatrix, pointRaw);
+
+            return new Point(pointTransformed[0,0], pointTransformed[1,0]);
+
+        }
+
+        private Point Scale(Point p, double scaleX, double scaleY)
+        {
+            double[,] scaleMatrix =
+            {
+                {scaleX, 0, 0 },
+                {0, scaleY, 0 },
+                {0, 0, 1 }
+            };
+
+            double[,] pointRaw =
+            {
+                { p.X },
+                { p.Y },
+                { 1 }
+            };
+
+            double[,] pointTransformed = MatrixMultiply(scaleMatrix, pointRaw);
+
+            return new Point(pointTransformed[0, 0], pointTransformed[1, 0]);
+        }
+
+        private void GenerateResultTransformMatrix(double units, double scale)
+        {
+            string output = "|X_New|";
+            output += $"|1, 0, {units} |";
+            output += $"|{scale}, {0}, {0} |    | x |\n";
+
+            output += "|Y_New| = | ";
+            output += $"0, 1, {units} | x | ";
+            output += $"0, {scale}, 0  | x | y |\n";
+
+            output += "|1|    | ";
+            output += $"{0},  {0},  {1}    |    | ";
+            output += $"{0},  {0},  {1}            |    | 1 |\n";
+
+            output += "\n\n";
+
+            double[,] translateMatrix =
+            {
+                {1, 0, units },
+                {0, 1, units },
+                {0, 0, 1 }
+            };
+
+            double[,] scaleMatrix =
+            {
+                {scale, 0, 0 },
+                {0, scale, 0 },
+                {0, 0, 1 }
+            };
+
+            double[,] res = MatrixMultiply(translateMatrix, scaleMatrix);
+
+            for(int i = 0; i < res.GetLength(0); ++i)
+            {
+                for(int k = 0; k < res.GetLength(1); ++k)
+                {
+                    output += $"{res[i, k]} ";
+                }
+                output += "\n";
+            }
+
+            resultTransformMatrix = output;
+        }
+
+        public void SendResultTransformMatrixToFile()
+        {
+            File.WriteAllText("result.txt", resultTransformMatrix);
+        }
+    }
+}
